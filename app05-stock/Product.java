85e26334ac1687270ef4aea963e6585b3a13a2cd
Hamood Ur Rab Jaffery <72038627+hamoodjaffery@users.noreply.github.com>
2020-11-19 13:56:06 +0000

    added missing files

---
+++ app05-stock/Product.java
@@ -0,0 +1,108 @@
+/**
+ * Details of products sold by Jaffery's Telecommunication
+ * Modified by Hamood Jaffery
+ * 04.Nov.2020
+ */
+public class Product
+{
+    // An identifying number for this product.
+    private int id;
+    // The name of this product.
+    private String name;
+    // The quantity of this product in stock.
+    private int quantity;
+
+    /**
+     * Constructor for objects of class Product.
+     * The initial stock quantity is zero.
+     * @param id The product's identifying number.
+     * @param name The product's name.
+     */
+    public Product(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+        quantity = 0;
+    }
+
+    /**
+     * @return The product's id.
+     */
+    public int getID()
+    {
+        return id;
+    }
+
+    /**
+     *return The product's name.
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    /**
+     *set The product's name.
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * @return The quantity in stock.
+     */
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    /**
+     * @return The id, name and quantity in stock.
+     */
+    public String toString()
+    {
+        return id + ": " +  name + " stock level: " + quantity;
+    }
+
+    /**
+     * Restock with the given amount of this product.
+     * The current quantity is incremented by the given amount.
+     * @param amount The number of new items added to the stock.
+     *               This must be greater than zero.
+     */
+    public void deliver(int amount)
+    {
+        if(amount > 0) 
+        {
+            quantity = quantity + amount;
+        }
+        else 
+        {
+            System.out.println("Attempt to restock " + name +
+                               " with a negative or zero amount: " + amount);
+        }
+    }
+
+    /**
+     * Sell one of these products.
+     * An error is reported if there appears to be no stock.
+     */
+    public void sell(int saleQuantity)
+    {        
+      if(saleQuantity > quantity) 
+      {
+            System.out.println("Only " + quantity + " " + name +
+                " in stock, but there were " +
+                saleQuantity + " ordered ");
+
+            quantity = 0;
+      }
+        else        
+        {
+            System.out.println(
+                "Selling " + saleQuantity + " of stock item: " + name);
+                quantity -= saleQuantity;
+      }
+    }
+}
---
+++ app05-stock/StockDemo.java
@@ -0,0 +1,129 @@
+import java.util.Random;
+/**
+ * Stock Demo class is for testing purpose only.
+ * Modified by Hamood Jaffery
+ * 04.Nov.2020
+ */
+public class StockDemo
+{
+    // The stock manager.
+    private StockManager manager;
+    
+    private Random generator = new Random();
+    
+    private int amount = 0;
+    
+
+ /**
+  * Create a StockManager and populate it with a few
+  * sample products.
+  */
+ public StockDemo(StockManager manager)
+ {
+        this.manager = manager;
+        
+        manager.addProduct(new Product(100, "Apple iPhone 11 Pro Max (128GB)"));
+        manager.addProduct(new Product(101, "Apple iPhone 11 (64GB)"));
+        manager.addProduct(new Product(102, "Apple iPhone X (128GB)"));
+        manager.addProduct(new Product(103, "Apple iPhone XR (64GB)"));
+        manager.addProduct(new Product(104, "Apple iPhone XS Max (256GB)"));
+        manager.addProduct(new Product(105, "Apple iPhone 12 (128GB)"));
+        manager.addProduct(new Product(106, "Apple iPhone 12 Pro (256GB)"));
+        manager.addProduct(new Product(107, "Sony Xperia XA2 Ultra (16GB)"));
+        manager.addProduct(new Product(108, "Sony Xperia XA2 Compact (8GB)"));
+        manager.addProduct(new Product(109, "Sony Xperia XA2 (16GB)"));
+        manager.addProduct(new Product(110, "Samsung Note 20 Ultra 5G (256GB)"));
+        manager.addProduct(new Product(111, "Samsung Note 20 (128GB)"));
+ }
+    
+ /**
+  * Provide a very simple demonstration of how a StockManager
+  * might be used. Details of one product are shown, the
+  * product is restocked, and then the details are shown again.
+  */
+ public void runDemo()
+ {
+        manager.printAllProducts();
+        
+        int noProducts = manager.numberProductsInStock();
+        
+        System.out.println("No of Products in Stock = " + noProducts);
+        
+        demoDeliverProducts();
+        demoSellProducts();
+        demoRenameProductsById();
+        demoRemoveProductsById();
+        demoPrintProductByName("Apple");
+        demoPrintProductByName("Samsung");
+        demoPrintProductByName("128GB");
+        demoPrintLowStock(1);
+        demoPrintLowStock(2);
+        demoPrintLowStock(3);
+ }
+  
+ 
+ private void demoSellProducts()
+ {
+     System.out.println("\nSelling all the products\n");
+     
+     for(int id = 100; id <= 111; id++)
+     {
+          amount = generator.nextInt(8);
+          manager.sellProduct(id, amount);
+     }          
+                
+     manager.printAllProducts();
+ }
+    
+ private void demoDeliverProducts()
+ {
+      System.out.println("\nDelivering all the products\n");
+        
+      for(int id = 100; id <= 111; id++)
+      {
+          // this will throw error when amount = 0.
+          amount = generator.nextInt(8);
+          manager.delivery(id, amount);
+      }
+        
+      manager.printAllProducts();
+ }
+   
+ private void demoRenameProductsById() 
+ {
+       System.out.println("\nRename a product\n");
+       
+       String newName = "NewName";
+       int idToRename = generateRandomId(100, 11);
+       System.out.println("\nRenaming product with ID: " + idToRename + " to new name " + newName);
+       manager.renameProduct(idToRename, newName);
+       manager.printAllProducts();
+ }
+   
+ private void demoRemoveProductsById() 
+ {
+       int idToRemove = generateRandomId(100, 11);
+       System.out.println("\nRemoving product with ID: " + idToRemove);
+       manager.removeProduct(idToRemove);
+       manager.printAllProducts();
+ }
+   
+ private void demoPrintProductByName(String namePart) 
+ {
+       manager.printPrintProductByName(namePart);
+       
+       System.out.println("Printing Products By Name: ");
+ }
+   
+ private void demoPrintLowStock(int lowStockLevel)
+ {
+       manager.printPrintLowStock(lowStockLevel);
+       
+       System.out.println("Printing low stock products");
+ }
+   
+ private int generateRandomId(int min, int offset) 
+ {
+       return generator.nextInt(offset) + min;    
+ }
+}
